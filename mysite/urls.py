20f1f62b806cb4087f6fb2882fd68800861be488
sed <tumbaymbajamalumba@gmail.com>
2020-04-23 13:28:46 +0500

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import include, url
+from django.urls import path, include
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls)
+    url(r'^admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
